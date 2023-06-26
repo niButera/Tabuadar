4787422ce8d2a75a442e3ef58623259c2c46d9eb
niButera <nikolasxp683@gmail.com>
2023-06-26 15:38:10 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write(@"          --- Tabuada ---      
+
+Digite aqui o número desejado --> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int m = 0;
+int r;
+
+
+while (m <= 10)
+{
+    r = n * m;
+    Console.WriteLine($"{n} x {m} = {r}");
+    m += 1;
+}
+
